5858b4b466676e79587913a202aaf78d4ae50b54
danieljardine04 <danogaryjardine@gmail.com>
2021-11-26 22:54:35 -0700

    added the date to the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var today = moment().format('MMMM Do YYYY');
+var now = moment().format('h:mm:ss a');
+var currentHour = now - moment().format('mm:ss a');
 var textEl = document.getElementById("currentDay");
 textEl.textContent = today;
 var workHours = {};
-
